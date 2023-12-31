3a52f8350e2cd276b8effada3736f08e22c7c4dc
Nicolas <nicolas.hirab@gmail.com>
2023-12-31 17:20:40 -0500

    Added gatekeeper setup

---
+++ Gatekeeper/gatekeeper.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, request
+import requests
+import json
+
+app = Flask(__name__)
+
+PROXY_HOST = "10.0.1.8"
+
+@app.route('/read-query', methods=['POST'])
+def execute_read_query():
+    method_id = request.args.get('method_id')
+    query = request.get_json()['query']
+
+    url = f"http://{PROXY_HOST}:5000/read-query?method_id={method_id}"
+
+    payload = json.dumps({"query": query})
+
+    response = requests.request("POST", url, headers={'Content-Type': 'application/json'}, data=payload)
+
+    return response.text
+
+@app.route('/write-query', methods=['POST'])
+def execute_read_query():
+    query = request.get_json()['query']
+
+    url = f"http://{PROXY_HOST}:5000/write-query"
+
+    payload = json.dumps({"query": query})
+
+    response = requests.request("POST", url, headers={'Content-Type': 'application/json'}, data=payload)
+
+    return response.text
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
---
+++ Setup_Scripts/gatekeeper_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt install -y python3-pip
+
+git clone https://github.com/NicoHirab/LOG8415-Final-Project.git
+cd LOG8415-Final-Project/Gatekeeper
+
+pip install -r requirements.txt
+
+python gatekeeper.py
