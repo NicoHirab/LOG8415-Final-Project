1e0777ffa7f9a8c7a6a8944ba921db3dd11847ff
Nicolas <nicolas.hirab@gmail.com>
2024-01-01 21:34:55 -0500

    Modified Standalone setup

--- Setup_Scripts/standalone_mysql_setup.sh
+++ Setup_Scripts/standalone_mysql_setup.sh
@@ -12,3 +12,7 @@ tar -xvf /home/ubuntu/sakila-db.tar.gz -C /home/ubuntu/
 # Upload Sakila database to MySQL
 mysql -u root -e "SOURCE /home/ubuntu/sakila-db/sakila-schema.sql;"
 mysql -u root -e "SOURCE /home/ubuntu/sakila-db/sakila-data.sql;"
+
+
+mysql -u root -p"root" -e "CREATE USER 'root'@'%' IDENTIFIED BY 'root';"
+mysql -u root -p"root" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'; FLUSH PRIVILEGES;"
